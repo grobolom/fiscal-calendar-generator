dcbb800c008adf9c6d15a27f61601691002935c0
Vasja Volin <vasja.volin@gmail.com>
2016-04-10 15:00:55 -0400

    add store from pre-existing SFL project

---
+++ web/src/store.js
@@ -0,0 +1,44 @@
+var applyMiddleware = function (store, middlewares) {
+    middlewares = middlewares.slice();
+    middlewares.reverse();
+
+    var dispatch = store.dispatch;
+    middlewares.forEach(function (middleware) {
+        return dispatch = middleware(store)(dispatch);
+    });
+
+    return Object.assign({}, store, { dispatch: dispatch });
+}
+
+var createStore = function (reducer, initialState) {
+    var state;
+    var observers = [];
+
+    if (initialState != undefined) {
+        state = initialState;
+    }
+
+    var getState = function() {
+        return state;
+    };
+
+    var dispatch = function(action) {
+        state = reducer(state, action);
+        var listeners = observers;
+        for (var i = 0; i < listeners.length; i++) {
+            listeners[i]();
+        }
+    };
+
+    var subscribe = function(observer) {
+        observers.push(observer);
+    };
+
+    return {
+        getState: getState,
+        dispatch: dispatch,
+        subscribe: subscribe
+    };
+};
+
+export { applyMiddleware, createStore };
