5bbc0497414c896dabe6561c5634c683e4c694ec
Vasja Volin <vasja.volin@gmail.com>
2016-04-18 21:27:00 -0400

    add a first, stupid unit test

--- test/functions/testGetDays.js
+++ test/functions/testGetDays.js
@@ -1,1 +1,8 @@
 import expect from 'expect';
+import { getDays } from '../../web/src/functions/getDays.js';
+
+describe('getDays', function() {
+    it('should return 364 days', function() {
+        expect(getDays('445','2016-01-01').length).toEqual(364);
+    });
+});
