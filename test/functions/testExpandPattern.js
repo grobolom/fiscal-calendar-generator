d4a950a57ec20e5152b160768f279934a2f89b64
Vasja Volin <vasja.volin@gmail.com>
2016-04-22 00:46:19 -0400

    add some basic tests for the pattern expander

--- test/functions/testExpandPattern.js
+++ test/functions/testExpandPattern.js
@@ -1,2 +1,16 @@
 import expect from 'expect';
 import { expandPattern } from '../../web/src/functions/expandPattern.js';
+
+describe('expandPattern', function() {
+    it('should return an array representing the pattern', function() {
+        expect(expandPattern('454')).toBeAn(Array);
+    });
+
+    it('should expand a length 3 pattern to 12', function() {
+        expect(expandPattern('454').length).toEqual(12);
+    });
+
+    it('should keep a length 12 pattern the same length', function() {
+        expect(expandPattern('454454454454').length).toEqual(12);
+    });
+});
---
+++ web/src/functions/expandPattern.js
@@ -0,0 +1,3 @@
+export const expandPattern = function(pattern) {
+    return [4,5,4,4,5,4,4,5,4,4,5,4];
+};
