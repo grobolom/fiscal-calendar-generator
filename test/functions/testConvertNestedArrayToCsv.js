185300873d0c73a6a648a2bb2332011609aac460
Vasja Volin <vasja.volin@gmail.com>
2016-04-30 12:41:54 -0400

    changed convert function to allow for nested objects

--- test/functions/testConvertNestedArrayToCsv.js
+++ test/functions/testConvertNestedArrayToCsv.js
@@ -8,36 +8,20 @@ describe('convertNestedArrayToCsv', function() {
         }).toThrow(/not an array/);
     });
 
-    it('should throw an error if an inside element is not an array',
-        function() {
-            var badArray = [
-                ['good'],
-                ['good'],
-                'bad'
-            ];
-            expect(function() {
-                convertNestedArrayToCsv(badArray);
-            }).toThrow(/not a nested array/);
-        }
-    );
-
-    it('should throw an error if the nested arrays contain non-strings',
-        function() {
-            var badArray = [
-                ['good'],
-                ['good'],
-                [['awful']]
-            ];
-            expect(function() {
-                convertNestedArrayToCsv(badArray);
-            }).toThrow(/nested arrays are too deep/);
-        }
-    );
-
     it('should correctly convert simple nested array to csv', function() {
         var goodArray = [
             ['good','good'],
             ['fine','fine'],
+            ['nice','nice']
+        ];
+        expect(convertNestedArrayToCsv(goodArray))
+            .toEqual("good,good\r\nfine,fine\r\nnice,nice");
+    });
+
+    it('should correctly convert simple nested objects to csv', function() {
+        var goodArray = [
+            ['good','good'],
+            { 'wut': 'fine', 'but': 'fine' },
             ['nice','nice']
         ];
         expect(convertNestedArrayToCsv(goodArray))
--- web/src/functions/convertNestedArrayToCsv.js
+++ web/src/functions/convertNestedArrayToCsv.js
@@ -1,5 +1,51 @@
+// browser polyfill for object keys
+if (!Object.keys) {
+  Object.keys = (function() {
+    'use strict';
+    var hasOwnProperty = Object.prototype.hasOwnProperty,
+        hasDontEnumBug = !({ toString: null }).propertyIsEnumerable('toString'),
+        dontEnums = [
+          'toString',
+          'toLocaleString',
+          'valueOf',
+          'hasOwnProperty',
+          'isPrototypeOf',
+          'propertyIsEnumerable',
+          'constructor'
+        ],
+        dontEnumsLength = dontEnums.length;
+
+    return function(obj) {
+      if (typeof obj !== 'object' && (typeof obj !== 'function' || obj === null)) {
+        throw new TypeError('Object.keys called on non-object');
+      }
+
+      var result = [], prop, i;
+
+      for (prop in obj) {
+        if (hasOwnProperty.call(obj, prop)) {
+          result.push(prop);
+        }
+      }
+
+      if (hasDontEnumBug) {
+        for (i = 0; i < dontEnumsLength; i++) {
+          if (hasOwnProperty.call(obj, dontEnums[i])) {
+            result.push(dontEnums[i]);
+          }
+        }
+      }
+      return result;
+    };
+  }());
+}
+
 // not using array build-up and join because it's not worth it.
 // we have a maximum size and it's not very big
+//
+// we also decide to convert naively because we don't care to
+// account for crazy data, only the specific stuff we have now,
+// which is [{}, {}, {}]
 
 export const convertNestedArrayToCsv = function(nestedArray) {
     var resultString = '';
@@ -7,15 +53,16 @@ export const convertNestedArrayToCsv = function(nestedArray) {
         throw new Error('not an array!');
 
     nestedArray.forEach(function(element) {
-        if (!(element instanceof Array))
-            throw new Error('not a nested array!');
-
-        element.forEach(function(nestedElement) {
-            if (!(nestedElement instanceof String
-                        || typeof nestedElement === 'string' ))
-                throw new Error('nested arrays are too deep!');
-            resultString += nestedElement + ",";
-        });
+        if (element instanceof Array) {
+            element.forEach(function(nestedElement) {
+                resultString += nestedElement + ",";
+            });
+        } else {
+            var keys = Object.keys(element);
+            keys.forEach(function(key) {
+                resultString += element[key] + ",";
+            });
+        }
         resultString = resultString.substr(0, resultString.length - 1);
         resultString += "\r\n";
     });
@@ -24,3 +71,5 @@ export const convertNestedArrayToCsv = function(nestedArray) {
 
     return resultString;
 };
+
+
