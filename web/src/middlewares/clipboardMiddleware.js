743bb5db06e24b002bb387f978df7bffae20b5d6
Vasja Volin <vasja.volin@gmail.com>
2016-04-30 13:44:56 -0400

    fix copy action firing on every event instead of just copy/download

--- web/src/middlewares/clipboardMiddleware.js
+++ web/src/middlewares/clipboardMiddleware.js
@@ -3,13 +3,15 @@ import Clipboard from 'clipboard';
 var clipboardMiddleware = function(store) {
     return function (next) {
         return function (action) {
-            var clip = new Clipboard('#clip');
+            if (action.type === 'DOWNLOAD') {
+                var clip = new Clipboard('#clip');
 
-            var clipboard = document.getElementById('clip');
+                var clipboard = document.getElementById('clip');
 
-            var copyEvent = document.createEvent('Event');
-            copyEvent.initEvent('click', true, true);
-            clipboard.dispatchEvent(copyEvent);
+                var copyEvent = document.createEvent('Event');
+                copyEvent.initEvent('click', true, true);
+                clipboard.dispatchEvent(copyEvent);
+            }
         };
     }
 };
