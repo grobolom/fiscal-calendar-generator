57794d9690681b9f1abf32ab11e6367e72d90156
Vasja Volin <vasja.volin@gmail.com>
2016-04-29 23:37:25 -0400

    fix check for correct nesting of arrays

--- web/src/functions/convertNestedArrayToCsv.js
+++ web/src/functions/convertNestedArrayToCsv.js
@@ -4,16 +4,13 @@ export const convertNestedArrayToCsv = function(nestedArray) {
         throw new Error('not an array!');
 
     nestedArray.forEach(function(element) {
-        if (!(element instanceof Array)) {
-            console.log(element);
+        if (!(element instanceof Array))
             throw new Error('not a nested array!');
-        }
 
         element.forEach(function(nestedElement) {
-            if (!(nestedElement instanceof String)) {
-                console.log(nestedElement);
+            if (!(nestedElement instanceof String
+                        || typeof nestedElement === 'string' ))
                 throw new Error('nested arrays are too deep!');
-            }
         });
     });
 };
