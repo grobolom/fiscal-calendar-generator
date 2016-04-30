de7515f334ba146955640fc519afffc00f94c0b0
Vasja Volin <vasja.volin@gmail.com>
2016-04-30 13:30:49 -0400

    implement copy/download through syntetic event creation

--- web/src/middlewares/clipboardMiddleware.js
+++ web/src/middlewares/clipboardMiddleware.js
@@ -3,18 +3,13 @@ import Clipboard from 'clipboard';
 var clipboardMiddleware = function(store) {
     return function (next) {
         return function (action) {
-            console.log(action);
-            var clipboard = new Clipboard('#clip');
+            var clip = new Clipboard('#clip');
 
-            clipboard.on('success', function(e) {
-                console.log(e);
-            });
+            var clipboard = document.getElementById('clip');
 
-            clipboard.on('error', function(e) {
-                console.log(e);
-            });
-
-            clipboard.emit('click');
+            var copyEvent = document.createEvent('Event');
+            copyEvent.initEvent('click', true, true);
+            clipboard.dispatchEvent(copyEvent);
         };
     }
 };
