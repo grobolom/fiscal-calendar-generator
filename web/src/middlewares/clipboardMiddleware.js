1a07019ebda276f9e249556d26bd0af061cbd457
Vasja Volin <vasja.volin@gmail.com>
2016-05-01 18:24:30 -0400

    change middleware to respond to future selected download type

--- web/src/middlewares/clipboardMiddleware.js
+++ web/src/middlewares/clipboardMiddleware.js
@@ -5,23 +5,33 @@ import { convertNestedArrayToCsv } from '../functions/convertNestedArrayToCsv.js
 var clipboardMiddleware = function(store) {
     return function (next) {
         return function (action) {
+
+            var state = store.getState();
+            var downloadType = state.downloadType;
+
             if (action.type === 'DOWNLOAD') {
-                var clip = new Clipboard('#clip');
-                var clipboard = document.getElementById('clip');
-                var copyEvent = document.createEvent('Event');
-                copyEvent.initEvent('click', true, true);
+                switch (downloadType) {
+                    case 'clip':
+                        var clip = new Clipboard('#clip');
+                        var clipboard = document.getElementById('clip');
+                        var copyEvent = document.createEvent('Event');
+                        copyEvent.initEvent('click', true, true);
 
-                clipboard.dispatchEvent(copyEvent);
-            }
+                        clipboard.dispatchEvent(copyEvent);
+                        break;
+                    case 'csv':
+                        var content = [
+                            convertNestedArrayToCsv(store.getState().entries)
+                        ];
+                        var type = {type: "text/csv;charset=utf-8"};
+                        var blob = new Blob(content, type);
 
-            if (action.type === 'GENERATE') {
-                var content = [
-                    convertNestedArrayToCsv(store.getState().entries)
-                ];
-                var type = {type: "text/csv;charset=utf-8"};
-                var blob = new Blob(content, type);
-
-                saveAs(blob, "fiscal_calendar.csv");
+                        saveAs(blob, "fiscal_calendar.csv");
+                        break;
+                    default:
+                        console.log('fail!');
+                        break;
+                }
             }
 
             return next(action);
